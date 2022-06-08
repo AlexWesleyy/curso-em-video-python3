47308718ce21b9f1a320d7cef16d25c41e69f6f6
Alex Wesley <92955230+AlexWesleyy@users.noreply.github.com>
2022-06-08 09:14:41 -0300

    Respondendo ao usuário

---
+++ Mundo_1_Fundamentos/ex002.py
@@ -0,0 +1,7 @@
+"""
+Desafio 002
+Problema: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+Resolução do problema:
+"""
+nome=str(input('Digite seu nome: '))
+print(f'Bem-vindo {nome}!')
