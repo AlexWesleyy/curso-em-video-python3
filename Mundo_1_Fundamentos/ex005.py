e6fb4cfb656b3820f6a6412f434fe69dd9534f92
Alex Wesley <92955230+AlexWesleyy@users.noreply.github.com>
2022-06-08 09:18:50 -0300

    Antecessor e sucessor

---
+++ Mundo_1_Fundamentos/ex005.py
@@ -0,0 +1,10 @@
+"""
+Desafio 005
+Problema: Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+Resolução do problema:
+"""
+num = int(input('Informe um valor: '))
+
+sucessor = num + 1
+antecessor = num - 1
+print(f'Antecessor = {antecessor}\nInformado = {num}\nSucessor = {sucessor}')
