ea8f3b28e82fd12e1bd42cae09a0e3e9a251183a
Alex Wesley <92955230+AlexWesleyy@users.noreply.github.com>
2022-06-08 09:15:49 -0300

    Somando dois números

---
+++ Mundo_1_Fundamentos/ex003.py
@@ -0,0 +1,9 @@
+"""
+Desafio 003
+Problema: Crie um programa que leia dois números e mostre a soma entre eles.
+Resolução do problema:
+"""
+a=int(input('Digite o primeiro número: '))
+b=int(input('Digite o segundo número: '))
+soma=a+b
+print(f'{a}+{b}={soma}')
