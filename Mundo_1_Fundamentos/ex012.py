b309465f808d11b4647ce5a2486bf216c2cd3be2
Alex Wesley <92955230+AlexWesleyy@users.noreply.github.com>
2022-06-09 14:04:00 -0300

    Calculando descontos

---
+++ Mundo_1_Fundamentos/ex012.py
@@ -0,0 +1,10 @@
+"""
+Desafio 012
+Problema: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+Resolução do problema:
+"""
+preco = float(input('Informe o preço do produto: R$'))
+
+total = preco - (preco * 5 / 100)
+
+print(f'Total à pagar com 5% de desconto: R${total:.2f}')
