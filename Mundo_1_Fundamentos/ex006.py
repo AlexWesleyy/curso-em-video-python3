f7a85fd31ed4eb417723a5832bd88eadc3126deb
Alex Wesley <92955230+AlexWesleyy@users.noreply.github.com>
2022-06-09 13:57:21 -0300

    Dobro, Triplo e Raiz Quadrada

---
+++ Mundo_1_Fundamentos/ex006.py
@@ -0,0 +1,11 @@
+"""
+Desafio 006
+Problema: Crie um algoritmo que leia um número e mostre o seu drobro, triplo e raiz quadrada.
+Resolução do problema:
+"""
+num = int(input('Informe um valor: '))
+dobro = num * 2
+triplo = num * 3
+raiz = num ** (1/2)  # Também pode-se utilizar a função pow(), de sintaxe: pow(num, (1/2))
+
+print(f'Dobro: {dobro}\nTriplo: {triplo}\nRaiz Quadrada: {raiz:.2f}')
